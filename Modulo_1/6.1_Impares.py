e0631d599923661c8cb7183e33db62cade725f86
02mio200 <pedrojimenezmoises@gmail.com>
2024-06-19 22:27:08 -0600

    Progrma impares

---
+++ Modulo_1/6.1_Impares.py
@@ -0,0 +1,7 @@
+numero = int(input("Introduce un número: "))
+
+    # Verificar si el número es par o impar usando if
+    if numero % 2 == 0:
+        print(f"El número {numero} es par.")
+    else:
+        print(f"El número {numero} es impar.")
