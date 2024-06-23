a6605a9d78c155959e6b09d69f051fffe35a0bb5
02mio200 <pedrojimenezmoises@gmail.com>
2024-06-23 13:34:15 -0600

    Ejercicio sentencia for 10.1 funcion

--- Modulo_1/10.1.Ejercicio.py
+++ Modulo_1/10.1.Ejercicio.py
@@ -4,28 +4,19 @@
 
 # Instrucciones:
 # 1. Defina la función f(x) = x^2 - 6x + 3.
-
-def f(x):
-    return x**2 - 6*x + 3
-
 # 2. Inicialice una variable para almacenar el valor mínimo de la función (min_f) y otra para el valor de x correspondiente (min_x).
-
-min_f = float()
-min_x = None
-
 # 3. Use un bucle for para iterar sobre los valores enteros de x en el rango [-9, 9].
 # 4. Para cada valor de x, evalúe la función f(x).
 # 5. Compare el resultado con min_f. Si es menor, actualice min_f y min_x.
-
-for x in range(-9, 9):
-    valor_f = f(x)
-    if valor_f < min_f:
-        min_f = valor_f
-        min_x = x
-
-
 # 6. Imprima el valor de x que minimiza f(x) y el valor mínimo de f(x).
 
+min_f = None
+
+for x in range(-9, 10):
+    valor_f = x**2 - 6*x + 3
+    if min_f is None or valor_f < min_f:
+        min_f = valor_f
+        min_x = x
 
 # Imprimir el resultado
 print(f'El valor de x que minimiza f(x) es: {min_x}')
--- Modulo_1/10.2.Ejercicio.py
+++ Modulo_1/10.2.Ejercicio.py
@@ -10,10 +10,3 @@
 # 5. Imprima la lista ordenada de estudiantes con sus promedios.
 
 # Definir la lista de diccionarios con los estudiantes y sus calificaciones
-estudiantes = [
-    {"nombre": "Alice", "matematicas": 85, "literatura": 78, "ciencia": 92},
-    {"nombre": "Bob", "matematicas": 89, "literatura": 94, "ciencia": 85},
-    {"nombre": "Charlie", "matematicas": 72, "literatura": 67, "ciencia": 80},
-    {"nombre": "David", "matematicas": 90, "literatura": 88, "ciencia": 91},
-    {"nombre": "Eva", "matematicas": 88, "literatura": 76, "ciencia": 85}
-]
